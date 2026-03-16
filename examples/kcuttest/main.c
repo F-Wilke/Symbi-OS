7daa882a96c8d9d66f025c422f48006fde33451d
Fredrik Wilke <fwilke@bu.edu>
2026-03-16 15:54:09 -0400

    [add:] pthread regression test

--- examples/kcuttest/main.c
+++ examples/kcuttest/main.c
@@ -14,6 +14,11 @@
 #include "pfadaptor.kh"
 #include "evacuate.kh"
 
+#include <pthread.h>
+
+extern void* vmalloc_noprof(unsigned long size);
+extern void vfree(void* ptr);
+
 static inline unsigned long get_exc_page_fault_addr()
 {
   unsigned long val;
@@ -38,6 +43,20 @@ int stacktouch(void)
   int sum = 0;
 
   for (int i=0; i<n; i+=4096) { data[i]=0xff; sum += data[i]; }  
+  
+  return sum;
+}
+
+int heaptouch(void)
+{
+  const int PGSIZE=4096;
+  const int n=PGSIZE * 8;
+  volatile char *data = vmalloc_noprof(n);
+  int sum = 0;
+
+  for (int i=0; i<n; i+=4096) { data[i]=0xff; sum += data[i]; }  
+  
+  vfree((void*)data);
   
   return sum;
 }
@@ -77,6 +96,42 @@ void evacuate()
   assert(rc==0);
 }
 
+unsigned int counter = 0;
+pthread_barrier_t barrier;
+//lock for counter
+pthread_mutex_t counter_lock = PTHREAD_MUTEX_INITIALIZER;
+
+typedef struct {
+  unsigned int arg;
+  unsigned int num_increments;
+} thread_arg_t;
+
+
+void threadfn(thread_arg_t* argptr)
+{
+  int mypid = current_pid();
+  if (argptr->arg % 100 == 0) {
+    printf("\t\t\tthreadfn: in thread with arg=%u pid=%d\n", argptr->arg, mypid);
+  }
+  stacktouch();
+
+
+  pthread_barrier_wait(&barrier);
+
+  for (unsigned int i=0; i<argptr->num_increments; i++) {
+    pthread_mutex_lock(&counter_lock);
+    counter++;
+    pthread_mutex_unlock(&counter_lock);
+
+    heaptouch();
+  }
+  
+  
+  pthread_barrier_wait(&barrier);
+  
+  free(argptr);
+}
+
 // external kernel symbol forward declarations
 // "native" kernel symbols -- NOT kernel "extension" symbols will be resolved at load time
 extern int overflowuid;
@@ -90,13 +145,17 @@ int main(int argc, char **argv) {
   signed long yieldcnt=10;
   int evac=1;
   volatile void * _printk_ptr;
-  unsigned num_forks = 1000;
+  unsigned num_forks = 1000, num_spawns = 1000, num_increments = 1000;
   
   _printk_ptr = (void *)_printk;
   if (argc > 1) ssec     = atoi(argv[1]);
   if (argc > 2) bloop    = atol(argv[2]);
   if (argc > 3) yieldcnt = atol(argv[3]);
   if (argc > 4) evac     = atoi(argv[4]);
+  if (argc > 5) num_forks = atoi(argv[5]);
+  if (argc > 6) num_spawns = atoi(argv[6]);
+  if (argc > 7) num_increments = atoi(argv[7]);
+
   
   printf("%d: BASIC KCUT TESTS: BEGIN: ssec=%d bloop=%lu yieldcnt=%lu\n", mypid, ssec, bloop, yieldcnt);
 
@@ -168,6 +227,8 @@ int main(int argc, char **argv) {
   for (unsigned i=0; i<num_forks; i++) {
     pid_t cpid = fork();
     if (cpid==0) {
+      if (i % 100 == 0) printf("\t\t%d: %lx: FORK TEST: in child at iteration %lu\n", mypid, cr3, i);
+      int mypid = current_pid(); //indirect elevate test: current_pid() is a kernel extension symbol that will only work if we are properly elevated in the child after fork
       stacktouch();
       exit(0);
     } else if (cpid<0) {
@@ -176,6 +237,39 @@ int main(int argc, char **argv) {
     }
   }
   printf("\t\t%d: %lx: FORK TEST: END\n", mypid , cr3);
+
+
+  printf("\t\t%d: %lx: PTHREAD TEST: spawning %lu times with stack touches\n", mypid, cr3, num_spawns);
+  pthread_barrier_init(&barrier, NULL, num_spawns);
+  
+  pthread_t* threads = malloc(sizeof(pthread_t)*num_spawns);
+
+  for (unsigned i=0; i<num_spawns; i++) {
+    thread_arg_t* argptr = malloc(sizeof(thread_arg_t));
+    argptr->arg = i;
+    argptr->num_increments = num_increments;
+    int rc = pthread_create(&threads[i], NULL, (void *(*)(void *))threadfn, (void *)argptr);
+    if (rc) {
+      printf("\t\t%d: %lx: PTHREAD TEST: pthread_create failed at iteration %lu with rc=%d\n", mypid, cr3, i, rc);
+      break;
+    }
+  }
+  //join all threads before exiting
+  for (unsigned i=0; i<num_spawns; i++) {
+    int rc = pthread_join(threads[i], NULL);
+    if (rc) {
+      printf("\t\t%d: %lx: PTHREAD TEST: pthread_join failed at iteration %lu with rc=%d\n", mypid, cr3, i, rc);
+      break;
+    }
+  }
+  pthread_barrier_destroy(&barrier);
+
+  free(threads);  
+  //check counter value to make sure threads ran properly
+  printf("\t\t%d: %lx: PTHREAD TEST: counter value is %u, expected %lu\n", mypid, cr3, counter, (unsigned long)num_spawns*num_increments);
+
+  printf("\t\t%d: %lx: PTHREAD TEST: END\n", mypid, cr3);
+
 
   
   sym_lower();
