62078416a4d823dff9068b4c76ac6c1754e530dd
Arlo Albelli <arloalb@bu.edu>
2026-03-20 23:59:16 -0400

    checkpointing code

--- examples/kcuttest/idtadaptor.h
+++ examples/kcuttest/idtadaptor.h
@@ -5,4 +5,20 @@ gate_desc * idt_adaptor_clone(void);
 void        idt_adaptor_activate(const gate_desc *idt);
 void        idt_adaptor_release(const gate_desc *idt);
 
+static inline void
+read_gate(gate_desc *idt, int n, gate_desc *gate)
+{
+  write_idt_entry(gate, 0, &(idt[n])); 
+}
+
+static inline void
+gate_set_offset(gate_desc *gate, unsigned long addr)
+{
+  gate->offset_low      = (u16) addr;
+  gate->offset_middle   = (u16) (addr >> 16);
+#ifdef CONFIG_X86_64
+  gate->offset_high     = (u32) (addr >> 32);
+#endif
+}
+
 #endif
