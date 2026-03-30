ed57d8ae6feb70bf0876ba478fa2b5a530c45d5e
Fredrik Wilke <fwilke@bu.edu>
2026-03-29 21:56:34 -0400

    [add:] memcached next steps

---
+++ examples/memcached/memcached.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#set -x
+MY_PATH=$(realpath $0)
+
+SYMBIPATH=${MY_PATH%%/Symbi-OS/*}
+SYMBIPATH=${SYMBIPATH}/Symbi-OS
+SYMLIBPATH=${SYMBIPATH}/Symlib/dynam_build
+MEMCACHEDPATH=${SYMBIPATH}/examples/memcached
+KALLSYMSPATH=${MEMCACHEDPATH}/kcut
+
+SYMLIB=${SYMLIBPATH}/libSym.so
+KALLSYMLIB=${KALLSYMSPATH}/libkallsyms.so
+MEMCACHEDSERVER=${MEMCACHEDPATH}/memcached/memcached
+
+[[ ! -d $SYMBIPATH ]] && {
+    echo "ERROR: you are not in a subdir of Symbi-OS"
+    exit -1
+}
+
+[[ ! -a $SYMLIB ]] && {
+    echo "ERROR: could not find $SYMLIB -- try make in $SYMLIBPATH"
+    exit -1
+}
+
+[[ ! -a ${KALLSYMLIB} ]] && {
+    if [[ -a /proc/libkallsyms.so ]]; then
+	sudo cp /proc/libkallsyms.so ${KALLSYMLIB}
+    else
+	echo "ERROR: could find $KALLSYMLIB or /proc/libkallsyms.so"
+	exit -1
+    fi
+}
+
+
+[[ ! -a ${MEMCACHEDSERVER} ]] && {
+     echo "ERROR: could not find $MEMCACHEDSERVER"
+     exit -1
+ }
+
+echo "RUNNING: LD_DEBUG=files LD_LIBRARY_PATH=${SYMLIBPATH}:${KALLSYMSPATH} $MEMCACHEDSERVER -u root $@" > /dev/stderr
+
+LD_DEBUG=files LD_LIBRARY_PATH=${SYMLIBPATH}:${KALLSYMSPATH} $MEMCACHEDSERVER -u root $@
+
