0322ab42b03ee32745ce26bea460fc0001f78253
Fredrik Wilke <fwilke@bu.edu>
2026-04-06 15:44:39 -0400

    [fix:] memcahced run script

--- examples/memcached/memcached.sh
+++ examples/memcached/memcached.sh
@@ -12,6 +12,18 @@ SYMLIB=${SYMLIBPATH}/libSym.so
 KALLSYMLIB=${KALLSYMSPATH}/libkallsyms.so
 MEMCACHEDSERVER=${MEMCACHEDPATH}/memcached/memcached
 
+#check first arguments, append to MEMCACHEDSERVER if exists
+[[ -n $1 ]] && MEMCACHEDSERVER="$MEMCACHEDSERVER-$1"
+
+#define variable that holds all other arguments, except the first one
+MEMCACHEDARGS=" -u root"
+for arg in "$@"; do
+    if [[ "$arg" != "$1" ]]; then
+        MEMCACHEDARGS="$MEMCACHEDARGS $arg"
+    fi
+done
+
+
 [[ ! -d $SYMBIPATH ]] && {
     echo "ERROR: you are not in a subdir of Symbi-OS"
     exit -1
@@ -37,7 +49,7 @@ MEMCACHEDSERVER=${MEMCACHEDPATH}/memcached/memcached
      exit -1
  }
 
-echo "RUNNING: LD_DEBUG=files LD_LIBRARY_PATH=${SYMLIBPATH}:${KALLSYMSPATH} $MEMCACHEDSERVER -u root $@" > /dev/stderr
+echo "RUNNING: LD_DEBUG=files LD_LIBRARY_PATH=${SYMLIBPATH}:${KALLSYMSPATH} $MEMCACHEDSERVER $MEMCACHEDARGS" > /dev/stderr
 
-LD_DEBUG=files LD_LIBRARY_PATH=${SYMLIBPATH}:${KALLSYMSPATH} $MEMCACHEDSERVER -u root $@
+LD_DEBUG=files LD_LIBRARY_PATH=${SYMLIBPATH}:${KALLSYMSPATH} $MEMCACHEDSERVER $MEMCACHEDARGS
 
